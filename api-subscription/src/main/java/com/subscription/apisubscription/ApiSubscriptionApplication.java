a4fcdf6081514479a0267638076b871458e5c4ce
Tiago Pereira <tiago.pereira@hotmart.com>
2025-08-09 11:44:29 -0300

    Update project

---
+++ api-subscription/src/main/java/com/subscription/apisubscription/ApiSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.subscription.apisubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSubscriptionApplication.class, args);
+    }
+
+}
---
+++ api-subscription/src/test/java/com/subscription/apisubscription/ApiSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subscription.apisubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
